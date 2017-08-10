f68d774d4c7e6aefa3c88453fd538360ecd5b776
Scofield Hsu <scofield.hsu@gmail.com>
2017-08-10 18:41:22 +0000

    add use "SwiftCModule"

--- Package.swift
+++ Package.swift
@@ -6,6 +6,7 @@ let package = Package(
     name: "use_swift_module",
     dependencies: [
         .Package(url: "https://github.com/scofieldhsu/swift_module1.git", majorVersion: 1, minor: 0),
+	.Package(url: "https://github.com/scofieldhsu/swift_c_module.git", majorVersion: 1, minor: 0),
     ]
 )
 
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,9 +1,17 @@
 import swift_module1;
+import CSwiftScofieldLinux;
 
 
+/* swift_module1 */
 var struct1 = Scofield();
 struct1.hi();
 
 struct1 = Scofield("Orz");
 struct1.hi();
+
+
+/* CSwiftScofieldLinux */
+var return_value = swift_scofield_test(9);
+print("return_value: \(return_value)");
+
 
